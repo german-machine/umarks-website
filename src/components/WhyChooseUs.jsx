c60c35bb5b56a39733495f009d27f5e0735a021a
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-25 13:30:53 +0100

    changed the background color to blue and text color of the extensive network box to white

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -14,15 +14,15 @@ const WhyChooseUs = () => {
 
     function Works({ item }) {
         return (
-            <div className={`lg:w-[32%] max-w-max w-full md:w-[90%] ${item.id === 'why-us' ? 'bg-transparent' : 'bg-white'} mb-[17px] rounded-[10px] p-[20px_20px]`}>
+            <div className={`lg:w-[32%] max-w-max w-full md:w-[90%] ${item.id === 'why-us' ? 'bg-transparent' : item.id === '02' ? 'bg-[#020181]' : 'bg-white'} mb-[17px] rounded-[10px] p-[20px_20px]`}>
                 <div>
-                    <h1 className={`${item.id === 'why-us' ? 'md:text-[40px] text-[35px] leading-[50px] text-[#F87516]' : 'md:text-[45px] text-[40px] text-[#020181]'} font-[700] `}>{item.num}</h1>
+                    <h1 className={`${item.id === 'why-us' ? 'md:text-[40px] text-[35px] leading-[50px] text-[#F87516]' : item.id === '02' ? 'text-white text-[40px]' : 'md:text-[45px] text-[40px] text-[#020181]'} font-[700] `}>{item.num}</h1>
                 </div>
                 <div>
-                    <h1 className="text-[23px] p-[3px_0px] text-[#5C5C5C] font-semibold">{item.label}</h1>
+                    <h1 className={`text-[23px] p-[3px_0px] ${item.id === '02' ? 'text-white' : 'text-[#5C5C5C]'} font-semibold`}>{item.label}</h1>
                 </div>
                 <div>
-                    <p className={`${item.id ===  'why-us' ? 'text-[#000]' : 'text-[#787878]'}  font-[400] max-w-[100%]`}>{item.content}</p>
+                    <p className={`${item.id ===  'why-us' ? 'text-[#000]' : item.id === '02' ? 'text-white' : 'text-[#787878]'}  font-[400] max-w-[100%]`}>{item.content}</p>
                 </div>
             </div>
         )
